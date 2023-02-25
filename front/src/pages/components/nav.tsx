cc8e40851c5f9ab7ef053f8a24b25d2b3277e537
Bruno Wotzke <brunoricardowotzke@gmail.com>
2023-02-25 18:45:19 -0300

    .

--- front/src/pages/components/nav.tsx
+++ front/src/pages/components/nav.tsx
@@ -1,15 +1,11 @@
-import Styles from "../../styles/nav.module.scss"
+import SVG_back from "./SVG_voltar"
 
+import Styles from "../../styles/nav.module.scss"
 export default function Nav():JSX.Element{
     return(
         <nav id={Styles.nav}>
             <div className={Styles.userOut}>
-                <div className={Styles.fcon}>
-                    <div className={Styles.imageOut}>
-
-                    </div>
-                    <h3 className={Styles.nameInfo}>Bruno Wotzke</h3>
-                </div>
+                <div className={Styles.imageOut}></div>
             </div>
         </nav>
     )
